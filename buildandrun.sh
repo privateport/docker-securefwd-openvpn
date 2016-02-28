745f65c673f3e4910d5c0fe26b8ee24ed7f2e23a
SneakyScampi <admin@privateport.io>
2016-02-28 21:12:45 +0000

    fix config for securefwd

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,4 +1,4 @@
 docker stop securefwd-openvpn
 docker rm securefwd-openvpn
 docker build -t privateport/securefwd-openvpn .
-docker run -p 1194:1194/udp --rm -v /docker.persistant/securefwd-openvpn:/persistant --hostname securefwd-openvpn --name securefwd-openvpn -it --net=rednet --cap-add=NET_ADMIN --device /dev/net/tun privateport/securefwd-openvpn $1 $2 $3 $4 $5 $6
+docker run -p 1194:1194/udp --rm -v /docker.persistant/securefwd-openvpn:/persistant --hostname securefwd-openvpn --name securefwd-openvpn -it --net=rednet --cap-add=NET_ADMIN --device /dev/net/tun privateport/securefwd-openvpn --sfwd-email blue@securefwd.io --sfwd-apikey abc --sfwd-port 12345 $1 $2 $3 $4 $5 $6
--- start.sh
+++ start.sh
@@ -2,23 +2,30 @@
 
 function print_help {
 cat <<EOF
-      		OpenVPN Server
-===============================================
-
-docker run sneakyscampi/docker-openvpn-sec (OPTIONS)
+Misc Options:
         -h | --help             	Print this help
+	-x | --debug			Drop into a shell
+---------------------------------------------
+Configure Options:
         -i | --init			Create CA, Server certs, Openvpn config (-d required)
         -d | --domainname       	Domainname
         -c | --createclient     	Create Client Certificate (-n required)
 	-o | --createclientcert-ovpn	Create Client Certificate and get ovpn client config.
 	-n | --commonName		Common Name
 	-g | --getclientcert-ovpn	Get preexisiting Client Certificate with OVPN
+
+---------------------------------------------
+Start Options: 
 	-s | --start			Start OpenVPN
-	
+	-f | --securefwd		Enabled Secure Fwd (securefwd.io)
+		--sfwd-email		Securefwd.io Username/Email
+		--sfwd-apikey		Securefwd.io API Key
+		--sfwd-port		Requested Port on Securefwd.io
 _______________________________________________
 by SneakyScampi
 EOF
 }
+
 if [ $# -eq 0 ]; then
     print_help
     exit 1
@@ -26,8 +33,7 @@ fi
 
 SSLCONFIGDIR=/etc/openssl
 
-
-OPTS=`getopt -o hid:con:xgs --long help,init,domainname:,client,createclientcert-ovpn,commonName:,debug,getclientcert-ovpn,start -n 'parse-options' -- "$@"`
+OPTS=`getopt -o hid:con:xgsf --long help,init,domainname:,client,createclientcert-ovpn,commonName:,debug,getclientcert-ovpn,start,securefwd,sfwd-email:,sfwd-apikey:,sfwd-port: -n 'parse-options' -- "$@"`
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 
 echo #OPTS
@@ -43,6 +49,10 @@ while true; do
 	-x | --debug )		DEBUG=true; shift ;;
         -g | --getclientcert-ovpn ) GETOVPN=true; shift ;;
 	-s | --start )		START=true; shift ;;
+	-f | --securefwd ) 	SECUREFWD=true; shift ;;
+        --sfwd-email )     SFWDEMAIL="$2"; shift; shift ;;
+        --sfwd-apikey )     SFWDAPIKEY="$2"; shift; shift ;;
+        --sfwd-port )     SFWDPORT="$2"; shift; shift ;;
         -- ) shift; break ;;
         * ) break ;;
   esac
@@ -51,6 +61,7 @@ done
 if [ ! -d "/persistant/openssl" ]; then
 	mkdir -p /persistant/openssl
 fi
+
 ln -s /persistant/openssl /etc/openssl
 
 if [ -n "$DEBUG" ]; then
@@ -102,12 +113,27 @@ if [ -n "$START" ]; then
 	echo "Starting Openvpn..."
 	build-openvpn-config
 	track-changes-etchosts-dns-openvpn-conf.sh&
-	if [ ! -d "/persistant/openssl" ]; then
-		mkdir -p /persistant/openssl
+	
+	if [ -n "$SECUREFWD" ]; then
+		if [ -z "$SFWDEMAIL" ] || [ -z "$SFWDAPIKEY" ] || [ -z "$SFWDPORT" ] ; then
+			echo "Error: Securefwd chosen but there are missing required securefwd parameters...."
+			echo "SFWDEMAIL: $SFWDEMAIL"
+			echo "SFWDAPIKEY: $SFWDAPIKEY"
+			echo "SFWDPORT: $SFWDPORT"
+			exit 1
+		fi
+		#So it's OK to Proceed
+		if [ ! -d "/persistant/nastpunchc" ]; then
+			mkdir -p /persistant/natpunchc
+		fi
+	
+		#Write Config to Persistant Storage
+		echo "{ \"email\": \"$SFWDEMAIL\", \"api\": \"$SFWDAPIKEY\", \"port\": \"$SFWDPORT\" }" > /persistant/natpunchc/config.json
+
+		#Get the latest code
+		cd /root/natpunchc && git pull
+		npm update && npm start &
 	fi
-	ln -s /persistant/openssl /etc/openssl
-	cd /root/natpunchc && git pull
-	npm start &
 	# We do the following as track-changes-daemon above will kill openvpn so it restarts if the dns changes IP.
 	# I Should actually let docker restart this, I need to investigate how this works later, probably using compose.
 	while true; do
